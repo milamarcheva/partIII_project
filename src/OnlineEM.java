29d37f0863394d1204378330114b35fbc29a3ed8
Mila Marcheva <mila.m.marcheva@gmail.com>
2022-04-07 17:01:56 +0100

    Added DataLoader class.
    The DataLoader class reads the files containing the PTB and saves the information in the array sentences.

--- src/OnlineEM.java
+++ src/OnlineEM.java
@@ -1,15 +1,19 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import javax.xml.crypto.Data;
+import java.util.Collections;
+import java.util.stream.*;
 
 public class OnlineEM {
 
 
-  private static void print2D(double[][] mat)
-  {
+  private static void print2D(double[][] mat) {
     // Loop through all rows
     for (double[] row : mat){
       System.out.println();
@@ -25,9 +29,11 @@ public class OnlineEM {
 
     if(a.length != b.length){
       System.out.println("Elementwise multiplication is not possible, because the number of rows differ");
+      System.out.println("a length: "+ a.length + "b lenght"+ b.length);
     }
     else if(a[0].length != b[0].length){
       System.out.println("Elementwise multiplication is not possible, because the number of cols differ");
+      System.out.println("a[0] length: "+ a[0].length + "b[0] lenght"+ b[0].length);
     }
     else{
       for (int i=0; i<a.length; i++){
@@ -39,11 +45,21 @@ public class OnlineEM {
     return res;
   }
 
+  private static double[][] multiply2Dscalar(double[][] a, int scalar){
+    double[][] res = new double[a.length][a[0].length];
+      for (int i=0; i<a.length; i++){
+        for (int j=0; j<a[0].length; j++){
+          res[i][j] = a[i][j]*scalar;
+        }
+      }
+    return res;
+  }
+
   private static double[][] divide2D1D(double[][] a, double[] b){
     double[][] res = new double[a.length][a[0].length];
 
     if(a[0].length != b.length){ //Columns in a should be the same number as elements in b
-      System.out.println("Elementwise multiplication is not possible, because the number of rows differ");
+      System.out.println("Elementwise division is not possible, because the number of rows differ");
       System.out.println("a length: "+ a.length + "b lenght"+ b.length);
     }
     else{
@@ -79,7 +95,8 @@ public class OnlineEM {
     double[] res = new double[a.length];
 
     if(a.length != b.length){
-      System.out.println("Elementwise multiplication is not possible, because the number of elements differ");
+      System.out.println("Elementwise division is not possible, because the number of elements differ");
+      System.out.println("a length: "+ a.length + "b lenght"+ b.length);
     }
 
     else{
@@ -90,6 +107,24 @@ public class OnlineEM {
     return res;
   }
 
+  private static double[][] add2D2D(double[][] a, double[][] b){
+    double[][] res = new double[a.length][a[0].length];
+
+    if(a.length != b.length){
+      System.out.println("Elementwise addition is not possible, because the number of rows differ");
+    }
+    else if(a[0].length != b[0].length){
+      System.out.println("Elementwise addition is not possible, because the number of cols differ");
+    }
+    else{
+      for (int i=0; i<a.length; i++){
+        for (int j=0; j<a[0].length; j++){
+          res[i][j] = a[i][j]+b[i][j];
+        }
+      }
+    }
+    return res;
+  }
 
   private static class ReturnBM{
     private double[][] tp;
@@ -131,7 +166,7 @@ public class OnlineEM {
       forwardprob+=alpha[state][seq_len-1];
     }
 
-    System.out.println("Forward probability: " + forwardprob);
+//    System.out.println("Forward probability: " + forwardprob);
 
     return alpha;
   }
@@ -165,7 +200,7 @@ public class OnlineEM {
       backwardprob+=beta[state][0]*emission_p.get(seq.get(0)).get(state)*start_p[state];
     }
 
-    System.out.println("Backward probability: " + backwardprob);
+//    System.out.println("Backward probability: " + backwardprob);
 
     return beta;
   }
@@ -175,7 +210,7 @@ public class OnlineEM {
       HashMap<String, ArrayList<Double>> emission_p,
       double[][] transition_p,
       double[] start_p) {
-    System.out.println("Baum Welch");
+//    System.out.println("Baum Welch");
     int number_of_states = start_p.length;
     int seq_len = seq.size();
 
@@ -277,20 +312,43 @@ public class OnlineEM {
     return new ReturnBM(a, b);
   }
 
-  //TODO
-//  public static ReturnBM onlineEM(
-//      ArrayList<ArrayList<String>> sequences,
-//      HashMap<String, ArrayList<Double>> emission_p,
-//      double[][] transition_p,
-//      double[] start_p) {
-//    System.out.println("Online EM");
-//    int number_of_states = start_p.length;
-//    int seq_len = seq.size();
-//
-//    return new ReturnBM(tp, ep);
-//    }
 
-  public static void main(String[] args) {
+  public static double[] random1D(int size){
+    double[] res = new double[size];
+    for (int i = 0; i <size ; i++) {
+      res[i] = Math.random();
+    }
+    return res;
+  }
+  public static double[][] random2D(int size1, int size2){
+    double[][] res = new double[size1][size2];
+    for (int i = 0; i <size1 ; i++) {
+      for (int j = 0; j < size2; j++) {
+        res[i][j] = Math.random();
+      }
+    }
+    return res;
+  }
+
+  public static double[][] tpNormalization(double[][] transitionProbs){
+    int clustersNumber = transitionProbs.length;
+    double[] sumsTransitionProbs = new double[clustersNumber];
+    for (int i = 0; i <clustersNumber; i++) {
+      for (int j = 0; j <clustersNumber ; j++) {
+        sumsTransitionProbs[i]+=transitionProbs[i][j];
+      }
+    }
+    for (int i = 0; i < clustersNumber; i++) {
+      for (int j = 0; j <clustersNumber; j++) {
+        double numerator = transitionProbs[i][j];
+        transitionProbs[i][j] = numerator/sumsTransitionProbs[i];
+      }
+    }
+
+    return transitionProbs;
+  }
+
+  public static void simpleTest(){
     ArrayList<String> seq = new ArrayList<String>(Arrays.asList("C", "C","G", "A", "A", "G", "T", "G"));
     HashMap<String, ArrayList<Double>> emission_p = new HashMap<String, ArrayList<Double>>(){{
       put("A", new ArrayList<>(Arrays.asList(0.7, 0.3, 0.4)));
@@ -311,9 +369,157 @@ public class OnlineEM {
     ReturnBM rbm = baumWelch(seq, emission_p, transition_p, start_p);
 
     print2D(rbm.tp);
+  }
+
+  public static ReturnBM iterBaumWelch(ArrayList<ArrayList<ArrayList<String>>> sentences, int iters, int subsetSize){
+
+    //INITALIZATION OF MATRICES
+    int clustersNumber = 6;
+
+    double[] startProbs = random1D(clustersNumber);
+    double sumStartProbs = 0;
+    for(int i = 0; i <clustersNumber ; i++) {
+      sumStartProbs+=startProbs[i];
+    }
+    for(int i = 0; i <clustersNumber ; i++) {
+      double numerator = startProbs[i];
+      startProbs[i] = numerator/sumStartProbs;
+    }
+
+    double[][] transitionProbs = random2D(clustersNumber, clustersNumber);
+    transitionProbs = tpNormalization(transitionProbs);
+
+    //Deep Copy of sentences that is shuffled
+    Collections.shuffle(sentences);
+//    int subsetSize = 100;
+    ArrayList<ArrayList<ArrayList<String>>> croppedSentences =  new ArrayList<>();
+    for (int i = 0; i <subsetSize; i++) {
+      croppedSentences.add(sentences.get(i));
+    }
+
+    HashSet<String> vocabSentsSet = new HashSet<>();
+    for (ArrayList<ArrayList<String>> sent: sentences) {
+      for (ArrayList<String> tokenData : sent ) {
+        vocabSentsSet.add(tokenData.get(2));
+      }
+    }
+    ArrayList<String> vocabSents = new ArrayList<>(vocabSentsSet);
+
+    int T = vocabSents.size();
+    int Z = subsetSize;
+    int S = clustersNumber;
+
+    double[][][] M =  new  double[T][Z][S];
+
+    for (int z = 0; z < Z; z++) {
+      ArrayList<ArrayList<String>> sent = croppedSentences.get(z);
+      for (ArrayList<String> tokenData : sent) {
+        int t = vocabSents.indexOf(tokenData.get(2));
+
+        double[] stateCounts = random1D(S);
+        double sumStateCounts = 0;
+        for(int i = 0; i <S; i++) {
+          sumStateCounts+=stateCounts[i];
+        }
+
+        for(int i = 0; i <S; i++) {
+          M[t][z][i]+=stateCounts[i]/sumStateCounts;
+        }
+      }
+    }
+
+    // BAUM WELCH ITERATIONS
+    double[][] tpFinal = new double[S][S];
+    HashMap<String, ArrayList<Double>> epFinal = new HashMap<>();
+
+    for (int iter = 0; iter<iters; iter++) {
+      //
+      System.out.println("Iteration: "+ (iter+1) );
+
+      //Emission probs
+      HashMap<String, ArrayList<Double>> emissionProbs = new HashMap<>();
+
+      double[] denom = new double[S];
+      for (int t = 0; t < T; t++) {
+        for (int z =0; z < Z; z++) {
+          for (int s = 0; s <S; s++) {
+            denom[s]+=M[t][z][s];
+          }
+        }
+      }
+
+      for (int t = 0; t < T; t++) {
+        String tokenLowercase = vocabSents.get(t);
+        double[] numerator = new double[S];
+        for (int z =0; z < Z; z++) {
+          for (int s = 0; s <S; s++) {
+            numerator[s]+=M[t][z][s];
+          }
+        }
+        double[] emissionsPrimitive = divide1D(numerator, denom);
+        ArrayList<Double> emissions = DoubleStream.of(emissionsPrimitive).boxed().collect(Collectors.toCollection(ArrayList::new));
+
+        emissionProbs.put(tokenLowercase, emissions);
+      }
+
+      //Baum Welch
+      double[][] transitionProbsTotal = new double[S][S];
+      ArrayList<Integer> randomSenteneOrder = IntStream.range(0,Z).boxed().collect(Collectors.toCollection(ArrayList::new));
+      Collections.shuffle(randomSenteneOrder);
+
+      for (Integer z : randomSenteneOrder ) {
+        ArrayList<ArrayList<String>> sentAllData= croppedSentences.get(z);
+        ArrayList<String> sent  = new ArrayList<>();
+        for (ArrayList<String> tokenData: sentAllData) {
+          sent.add(tokenData.get(2));
+        }
+        ReturnBM rBM = baumWelch(sent, emissionProbs, transitionProbs, startProbs);
+
+        for (String word : sent) {
+          int t = vocabSents.indexOf(word);
+          for (int s = 0; s < S; s++) {
+            M[t][z][s] = 0;
+          }
+        }
+
+        for (String word : sent) {
+          int t = vocabSents.indexOf(word);
+          ArrayList<Double> epWord = rBM.ep.get(word);
+          for (int s = 0; s < S; s++) {
+            M[t][z][s] += epWord.get(s);
+          }
+        }
+        transitionProbsTotal = add2D2D(transitionProbsTotal,  multiply2Dscalar(rBM.tp, sent.size()));
+
+      }
+
+      transitionProbs = tpNormalization(transitionProbsTotal);
+
+
+      if(iter%5==0){
+        print2D(transitionProbs);
+      }
+
+      tpFinal = transitionProbs;
+      epFinal = emissionProbs;
+    }
+//    print2D(transitionProbs);
+
+    ReturnBM res = new ReturnBM(tpFinal, epFinal);
+    return res;
+  }
+  public static void main(String[] args) {
+    //simpleTest();
+
 
     ArrayList<ArrayList<ArrayList<String>>> sentences = DataLoader.populateSentences(DataLoader.PTB_PATH);
-//    System.out.println(sentences.get(3).get(3).get(2));
+    System.out.println(sentences.size() + " sentences were loaded.");
+    int iters = 20;
+    int subsetSize = 350;
+    ReturnBM rBM = iterBaumWelch(sentences, iters, subsetSize);
+    System.out.println(iters + " iterations over " + subsetSize + " sentences were completed. ");
+    System.out.println("Final transition probabilities: ");
+    print2D(rBM.tp);
 
   }
 
